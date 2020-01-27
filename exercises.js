a97669c564f8a6539a50ed85aea1e561b5e5ce62
Daniel Kent <Kentpercussion@gmail.com>
2020-01-27 09:48:51 -0700

    q 3

--- exercises.js
+++ exercises.js
@@ -8,7 +8,7 @@ starTrek.push('McCoy');
 starTrek.pop();
 
 function peek(stack) {
-  return stack.top.val
+  return stack.top.val;
 }
 
 function isEmpty(stack) {
@@ -21,8 +21,27 @@ function isEmpty(stack) {
 
 function display(stack) {
   let currNode = stack.top;
-  while(currNode != null) {
+  while(currNode !== null) {
     console.log(currNode.val);
     currNode = currNode.next;
   }  
 }
+display(starTrek);
+
+function isPalindrome(str) {
+  str = str.toLowerCase().replace(/[^a-zA-Z0-9]/g, '');
+  let reverse = '';
+  const stack = new Stack;
+  for (let i = 0; i < str.length; i++) {
+    stack.push(str[i]);
+  }
+  while (stack.top !== null) {
+    reverse += stack.top.val;
+    stack.pop();
+  }
+  if (str === reverse) return true;
+  return false;
+}
+
+console.log(isPalindrome('A man, a plan, a canal: Panama'));
+
