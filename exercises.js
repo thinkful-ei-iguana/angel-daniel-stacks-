90fff21e40f91b33c09b49f4266ffe3ea3c8a853
Angel Alicea <ala31996@gmail.com>
2020-01-27 11:29:22 -0500

    finished drill 2

--- exercises.js
+++ exercises.js
@@ -5,4 +5,24 @@ starTrek.push('Kirk');
 starTrek.push('Spock');
 starTrek.push('Scotty');
 starTrek.push('McCoy');
+starTrek.pop();
 
+function peek(stack) {
+  return stack.top.val
+}
+
+function isEmpty(stack) {
+  let top = stack.top;
+  if( top === null ) {
+    return true;
+  }
+  return false;
+}
+
+function display(stack) {
+  let currNode = stack.top;
+  while(currNode != null) {
+    console.log(currNode.val);
+    currNode = currNode.next;
+  }  
+}
