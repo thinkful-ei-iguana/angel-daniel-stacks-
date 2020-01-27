2e6e41d65ba063ca276d752a4b04e486c9381f37
Daniel Kent <Kentpercussion@gmail.com>
2020-01-27 09:15:50 -0700

    ex 1

---
+++ exercises.js
@@ -0,0 +1,8 @@
+const Stack = require('./stack');
+const starTrek = new Stack;
+
+starTrek.push('Kirk');
+starTrek.push('Spock');
+starTrek.push('Scotty');
+starTrek.push('McCoy');
+
--- stack.js
+++ stack.js
@@ -1,5 +1,22 @@
-class Stack {
+class _node {
+  constructor(val=null, next=null) {
+    this.val = val;
+    this.next = next;
+  }
+}
 
+class Stack {
+  constructor() {
+    this.front = null;
+  }
+
+  push(val) {
+    this.front = new _node(val, this.front);
+  }
+
+  pop() {
+    this.front = this.front.next;
+  }
 }
 
 module.exports = Stack;
